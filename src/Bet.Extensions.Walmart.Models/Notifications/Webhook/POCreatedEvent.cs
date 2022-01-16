241eb5df464500d6ccacf8341c6fc05688d3f12b
kdcllc <info@kingdavidconsulting.com>
2022-01-16 18:15:17 -0500

    updating webhooks and adding more events

---
+++ src/Bet.Extensions.Walmart.Models/Notifications/Webhook/POCreatedEvent.cs
@@ -0,0 +1,23 @@
+﻿namespace Bet.Extensions.Walmart.Models.Notifications.Webhook;
+
+/// <summary>
+/// Purchase Order (PO) Created Event.
+///
+/// This event occurs when there’s a new Purchase Order created for the seller.
+///
+/// <see href="https://developer.walmart.com/doc/us/us-mp/us-mp-notifications/"/>.
+/// </summary>
+public class POCreatedEvent
+{
+    /// <summary>
+    /// Event Meta information.
+    /// </summary>
+    [JsonPropertyName("source")]
+    public EventSource? Source { get; set; }
+
+    /// <summary>
+    /// Event details.
+    /// </summary>
+    [JsonPropertyName("payload")]
+    public POCreatedEventPayload? Payload { get; set; }
+}
---
+++ src/Bet.Extensions.Walmart.Models/Notifications/Webhook/POLineAutoCancelledEvent.cs
@@ -0,0 +1,26 @@
+﻿namespace Bet.Extensions.Walmart.Models.Notifications.Webhook;
+
+/// <summary>
+/// Purchase Order (PO) Line Auto-cancelled Event.
+///
+/// This notification occurs when a Purchase Order Line is auto-cancelled by Walmart
+///
+/// if order is not updated to Shipped status with a valid tracking number
+/// by Estimated Ship Date (ESD) + 8 calendar days
+///
+/// <see href="https://developer.walmart.com/doc/us/us-mp/us-mp-notifications/"/>.
+/// </summary>
+public class POLineAutoCancelledEvent
+{
+    /// <summary>
+    /// Event Meta information.
+    /// </summary>
+    [JsonPropertyName("source")]
+    public EventSource? Source { get; set; }
+
+    /// <summary>
+    /// Event details.
+    /// </summary>
+    [JsonPropertyName("payload")]
+    public POLineAutoCancelledEventPayload? Payload { get; set; }
+}
